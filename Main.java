075962f472522a120105e4badc842f4566d80789
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-10-11 00:15:27 -0300

    teste java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* teste */
+
+        int numero = 5;
+
+        System.out.println("O número da variável é o : " + numero);
+    }
+}
