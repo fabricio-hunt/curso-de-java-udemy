44657bf4e5a3677f6bf514394453e5d91f49b3f1
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-10-14 22:29:00 -0300

    estrutura sequencial - aula 1  e 2

---
+++ EstruturaSequencial.java
@@ -0,0 +1,19 @@
+public class EstruturaSequencial {
+    public static void main(String[] args) {
+        /*
+        ----------------------------------------------
+        Aula Expressões aritiméticas - ok
+
+        Variáveis Sintaxe:
+        <tipo> <nome> = <valor inicial>
+        int idade = 25;
+        char sexo = 'F';
+        Uma variável possui:
+        - nome
+        - tipo
+        - valor
+        - endereço
+        -----------------------------------------------
+         */
+    }
+}
