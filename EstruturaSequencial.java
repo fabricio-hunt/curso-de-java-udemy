ea5fc5ad1de71f59a9be9907fb69e37111f15dd6
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-10-18 00:47:55 -0300

    saída de dados em java

--- EstruturaSequencial.java
+++ EstruturaSequencial.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class EstruturaSequencial {
     public static void main(String[] args) {
         /*
@@ -15,5 +17,23 @@ public class EstruturaSequencial {
         - endereço
         -----------------------------------------------
          */
+
+        /* Saída de Dados */
+
+        String nome = "Jonas de Sá";
+        int idade = 31;
+        double matricula = 10.35784;
+        float renda = 4.500f;
+        Locale locale = new Locale("pt", "BR");
+
+
+
+        System.out.println("-------------------Seu Contracheque-----------------");
+        System.out.printf("Olá %s, sua idade é: %d anos, sua matricula é: %f e sua renda é: R$ %f reais%n", nome, idade, matricula, renda);
+
     }
 }
+
+
+
+
