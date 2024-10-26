c337c72ae556638a706a5cff9c71c33e6d36b153
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-10-25 23:07:59 -0300

    exercicio de soma

---
+++ PrimeiroExercicio1.java
@@ -0,0 +1,13 @@
+public class PrimeiroExercicio1 {
+    public static void main(String[] args) {
+        /* Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+        mensagem explicativa, conforme exemplos.  */
+
+        int primeiroNumero = 1500;
+        int segundoNumeroInteiro = 150;
+
+        int soma = (primeiroNumero + segundoNumeroInteiro);
+
+        System.out.println("A soma entre os dois números inteiros é: " + soma);
+    }
+}
