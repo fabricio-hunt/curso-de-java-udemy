fce8547c47fc7eb663e3a44c2e47c1c0a5a0906b
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-11-29 00:17:25 -0300

    estrutura do exercicio

---
+++ ExercicioFor1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ExercicioFor1 {
+    public static void main(String[] args) {
+        /* Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+        X, se for o caso. */
+
+        Scanner scanner = new Scanner(System.in);
+
+        //entrada de dados
+        System.out.println("Digite o valor de X: ");
+        int x = scanner.nextInt();
+
+        for(;;){
+            System.out.println("xxx");
+
+        }
+
+        //saida da condição
+
+    }
+
+}
+
+
