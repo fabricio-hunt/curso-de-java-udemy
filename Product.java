082d74296d986bcaf565e0e0c97c2257fb0e1e62
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-01-30 22:25:48 -0300

    novo exercicio

--- Product.java
+++ Product.java
@@ -1,5 +1,18 @@
 public class Product {
-    public static void main(String[] args) {
+        public String name;
+        public double price;
+        public int quantity;
 
+    public double totalValueInStock(){
+        return  price * quantity;
     }
+
+    public void  addProducts(int quantity){
+            this.quantity += quantity;
+    }
+
+    public void removeProducts( int quantity) {
+        this.quantity -= quantity;
+    }
+
 }
--- Program.java
+++ Program.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = scanner.nextLine();
+        System.out.print("Price: ");
+        product.price = scanner.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = scanner.nextInt();
+
+
+        scanner.close();
 
     }
 }
