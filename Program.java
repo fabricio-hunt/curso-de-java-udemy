981aaf1da2f248df13df928a759800e0b72dbbdb
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2025-02-07 21:29:53 -0300

    finalização do programa

--- Program.java
+++ Program.java
@@ -16,8 +16,21 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = scanner.nextInt();
 
-        System.out.println(product);
+        System.out.println();
+        System.out.println("Product data: " + product);
 
+        System.out.println();
+        System.out.println("Enter the number of products to be add in stock: ");
+        int quantity = scanner.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be remove from stock: ");
+        quantity = scanner.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
 
         scanner.close();
 
